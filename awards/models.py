2dd57b32571e4362cdd90296131bde63aeedc4bf
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-16 23:02:51 +0300

    method to save profile

--- awards/models.py
+++ awards/models.py
@@ -14,6 +14,9 @@ class Profile(models.Model):
       def __str__(self):
         return self.user.username
 
+      def save_profile(self):
+        self.save()
+
       def delete_profile(self):
         self.delete()  
  
