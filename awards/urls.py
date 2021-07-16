38a9c55b19044003df2b381f17e7626c497e4f1d
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-16 19:32:09 +0300

    Create local database

--- awards/urls.py
+++ awards/urls.py
@@ -8,5 +8,6 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     url(r'^$', views.index, name='index'),
+    url('registration/',views.registration, name='registration'),
     
 ]
