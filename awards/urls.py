ecbb973f61deec08c1c65add71b96d7371ba606b
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-16 14:15:24 +0300

    Add url for view index function

---
+++ awards/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+
+
+urlpatterns=[
+    url(r'^$', views.index, name='index'),
+    
+]
