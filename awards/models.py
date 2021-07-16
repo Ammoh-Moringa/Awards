61fe9bd36e4a4cb7ec6d3e55038b627ed84170a6
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-16 22:06:46 +0300

    Add class profile, make first migration to add the four fields to db

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
+class Profile(models.Model):
+      profile_picture = CloudinaryField('image')
+      bio = models.TextField(max_length=500, default="My Bio", blank=True)
+      contact = models.CharField(max_length=60,blank=True)
+      timestamp = models.DateTimeField(default=timezone.now())
+ 
--- awards/urls.py
+++ awards/urls.py
@@ -9,5 +9,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns=[
     url(r'^$', views.index, name='index'),
     url('registration/',views.registration, name='registration'),
+    url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(), name='logout'),
     
 ]
