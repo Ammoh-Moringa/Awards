69baf08d3bddd2938241241b60ccfdf7b266c67e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-17 11:10:33 +0300

    Add view function for profile

--- awards/views.py
+++ awards/views.py
@@ -1,7 +1,10 @@
-from awards.forms import SignUpForm
+from awards.models import Profile, Project
+from awards.forms import ProfileForm, SignUpForm
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -23,3 +26,20 @@ def registration(request):
     else:
         form= SignUpForm()
     return render(request, 'registration/registration_form.html', {"form":form}) 
+
+@login_required(login_url='/accounts/login/')
+def profile(request, username):
+    title = "Profile"
+    profile = User.objects.get(username=username)
+    users = User.objects.get(username=username)
+    id = request.user.id
+    form = ProfileForm()
+
+    try :
+        profile_info = Profile.get_by_id(profile.id)
+    except:
+        profile_info = Profile.filter_by_id(profile.id)
+
+
+    projects = Project.get_profile_pic(profile.id)
+    return render(request, 'registration/profile.html', {'title':title,'profile':profile,"projects":projects, 'profile_info':profile_info,"form":form})
